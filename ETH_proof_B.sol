c2f5e19963f4edb607fd3c3d357042a97c43beab
Aditya Singh <adityasingh98892111@gmail.com>
2024-06-13 11:26:00 +0530

    Update ETH_proof_B.sol

--- ETH_proof_B.sol
+++ ETH_proof_B.sol
@@ -1,1 +1,43 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract MyToken {
+    // Public variables for the token details
+    string public tokenName = "The_Name";
+    string public tokenAbbrv = "The_Abbrv";
+    uint public totalSupply = 0;
+
+    // Mapping of addresses to balances
+    mapping(address => uint) public balances;
+
+    // Mint function to increase total supply and the balance of the specified address
+    function mint(address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+
+    // Burn function to decrease total supply and the balance of the specified address
+    function burn(address _address, uint _value) public {
+        require(balances[_address] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_address] -= _value;
+    }
+}
